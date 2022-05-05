f2a508b2ef5f8e874aeb6098cb9eb1ad40b3a1ba
kkazu-hoge <928kuni@gmail.com>
2022-05-05 01:22:30 +0000

    [update]namespace view controller alter

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+
+  # 会員
+  # URLに"public"を含ませたくないためURLとファイル構成(public配下)が異なるscope moduleでルーティング定義
+  scope module: 'public' do
+    # root :to =>'homes#top'
+    # get "/about"=>'homes#about'
+    devise_for :customers,skip: [:passwords], controllers: { registrations: 'public/registrations'}
+  end
+
+  #管理者
+  # URLに"admin"にが含まれて問題ないためnamespaceでURLとファイル構成を同じように定義
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    # root :to =>'homes#top'
+    # get "/about"=>'homes#about'
+
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
