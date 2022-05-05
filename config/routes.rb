40f201f85e93a7f42608b36c43fac74bf990a581
kkazu-hoge <928kuni@gmail.com>
2022-05-05 08:12:33 +0000

    [update]side menu

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # 会員
   # URLに"public"を含ませたくないためURLとファイル構成(public配下)が異なるscope moduleでルーティング定義
   scope module: 'public' do
+    # root :to =>'registrations#new'
     # root :to =>'homes#top'
     # get "/about"=>'homes#about'
     devise_for :customers,skip: [:passwords], controllers: { registrations: 'public/registrations'}
