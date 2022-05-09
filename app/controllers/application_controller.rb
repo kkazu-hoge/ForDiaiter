1c2f00dfd5aaffe81f46abc6d224e697a04ddc57
kkazu-hoge <928kuni@gmail.com>
2022-05-09 09:32:25 +0000

    [update]header link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,30 +3,30 @@ class ApplicationController < ActionController::Base
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  # def after_sign_in_path_for(resource)
-  #   case resource
-  #   when Admin
-  #     admin_root_path
-  #   when Customer
-  #     root_path
-  #   end
-  # end
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_root_path
+    when Customer
+      root_path
+    end
+  end
 
-  # def after_sign_out_path_for(resource)
-  #   case resource
-  #   when :admin
-  #     new_admin_session_path
-  #   when :customer
-  #     new_customer_session_path
-  #   end
-  # end
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin
+      new_admin_session_path
+    when :customer
+      new_customer_session_path
+    end
+  end
 
-  # def after_sign_up_path_for(resource)
-  #   case resource
-  #   when Customer
-  #     customers_mypage_path
-  #   end
-  # end
+  def after_sign_up_path_for(resource)
+    case resource
+    when Customer
+      home_path
+    end
+  end
 
   # protected
   # def configure_permitted_parameters
