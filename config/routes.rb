1563ff2c000f0fbdcbcb5871d251f8b1bd4b580b
kkazu-hoge <928kuni@gmail.com>
2022-05-09 08:02:33 +0000

    [update]routing info

--- config/routes.rb
+++ config/routes.rb
@@ -64,3 +64,63 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+########################################
+########### ルーティング一覧############
+########################################
+
+#<会員(customer)>
+
+#           new_customer_session GET    /customers/sign_in(.:format)                    public/sessions#new
+#               customer_session POST   /customers/sign_in(.:format)                    public/sessions#create
+#       destroy_customer_session DELETE /customers/sign_out(.:format)                   public/sessions#destroy
+#   cancel_customer_registration GET    /customers/cancel(.:format)                     public/registrations#cancel
+#     new_customer_registration GET    /customers/sign_up(.:format)                    public/registrations#new
+#     edit_customer_registration GET    /customers/edit(.:format)                       public/registrations#edit
+#         customer_registration PATCH  /customers(.:format)                            public/registrations#update
+#                               PUT    /customers(.:format)                            public/registrations#update
+#                               DELETE /customers(.:format)                            public/registrations#destroy
+#                               POST   /customers(.:format)                            public/registrations#create
+#               customers_mypage GET    /customers/mypage(.:format)                     public/customers#show
+#     edit_mail_address_customer GET    /customers/:id/edit_mail_address(.:format)      public/customers#edit_mail_address
+#         edit_password_customer GET    /customers/:id/edit_password(.:format)          public/customers#edit_password
+#   edit_physical_info_customer GET    /customers/:id/edit_physical_info(.:format)     public/customers#edit_physical_info
+#   update_mail_address_customer PATCH  /customers/:id/update_mail_address(.:format)    public/customers#update_mail_address
+#       update_password_customer PATCH  /customers/:id/update_password(.:format)        public/customers#update_password
+# update_physical_info_customer PATCH  /customers/:id/update_physical_info(.:format)   public/customers#update_physical_info
+#           unsubscribe_customer GET    /customers/:id/unsubscribe(.:format)            public/customers#unsubscribe
+#                               GET    /customers/:id/unsubscribe(.:format)            public/customers#unsubscribe
+#             defection_customer PATCH  /customers/:id/defection(.:format)              public/customers#defection
+#                 edit_customer GET    /customers/:id/edit(.:format)                   public/customers#edit
+#                       customer PATCH  /customers/:id(.:format)                        public/customers#update
+#                               PUT    /customers/:id(.:format)                        public/customers#updat
+
+
+#<管理者(admin)>
+
+#         new_admin_session GET    /admin/sign_in(.:format)                admin/sessions#new
+#             admin_session POST   /admin/sign_in(.:format)                admin/sessions#create
+#     destroy_admin_session DELETE /admin/sign_out(.:format)               admin/sessions#destroy
+# cancel_admin_registration GET    /admin/cancel(.:format)                 admin/registrations#cancel
+#   new_admin_registration GET    /admin/sign_up(.:format)                admin/registrations#new
+#   edit_admin_registration GET    /admin/edit(.:format)                   admin/registrations#edit
+#       admin_registration PATCH  /admin(.:format)                        admin/registrations#update
+#                           PUT    /admin(.:format)                        admin/registrations#update
+#                           DELETE /admin(.:format)                        admin/registrations#destroy
+#                           POST   /admin(.:format)                        admin/registrations#create
+#               admin_root GET    /admin(.:format)                        admin/homes#home
+#   admin_customerss_index GET    /admin/customerss(.:format)             admin/customerss#index
+#     edit_admin_customerss GET    /admin/customerss/:id/edit(.:format)    admin/customerss#edit
+#         admin_customerss PATCH  /admin/customerss/:id(.:format)         admin/customerss#update
+#                           PUT    /admin/customerss/:id(.:format)         admin/customerss#update
+#             admin_columns GET    /admin/columns(.:format)                admin/columns#index
+#         edit_admin_column GET    /admin/columns/:id/edit(.:format)       admin/columns#edit
+#             admin_column PATCH  /admin/columns/:id(.:format)            admin/columns#update
+#                           PUT    /admin/columns/:id(.:format)            admin/columns#update
+#           admin_trainings GET    /admin/trainings(.:format)              admin/trainings#index
+#                           POST   /admin/trainings(.:format)              admin/trainings#create
+#       edit_admin_training GET    /admin/trainings/:id/edit(.:format)     admin/trainings#edit
+#           admin_training PATCH  /admin/trainings/:id(.:format)          admin/trainings#update
+#                           PUT    /admin/trainings/:id(.:format)          admin/trainings#update
+#                           DELETE /admin/trainings/:id(.:format)          admin/trainings#destroy
+
