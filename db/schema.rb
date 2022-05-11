53edc91bdce77856b0c2213a679552bc9acdaf49
kkazu-hoge <928kuni@gmail.com>
2022-05-11 00:13:46 +0000

    [update]home layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_10_052548) do
+ActiveRecord::Schema.define(version: 2022_05_10_231528) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -147,6 +147,7 @@ ActiveRecord::Schema.define(version: 2022_05_10_052548) do
     t.integer "interval", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
   end
 
   create_table "trainings", force: :cascade do |t|
