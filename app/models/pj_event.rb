570a9e17def234ecb9a9293ac9fa4e64b2541800
kkazu-hoge <928kuni@gmail.com>
2022-05-11 08:16:59 +0000

    [update]model asossiation bag fix

--- app/models/pj_event.rb
+++ app/models/pj_event.rb
@@ -1,7 +1,9 @@
 class PjEvent < ApplicationRecord
 
-  belongs_to :project, foreign_key => "project_id"
+  belongs_to :project
+  # , foreign_key => "project_id"
   has_many :pj_event_details, dependent: :destroy
+  # , foreign_key: "pj_event_id"
   has_many :trainings, through: :pj_event_details
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,7 @@ class Project < ApplicationRecord
   belongs_to :life_stress_factor
   has_many :pj_events, dependent: :destroy
   has_many :plan_pj_events, dependent: :destroy
+
   enum sex: { man: 1, woman: 2 }
 
 end
