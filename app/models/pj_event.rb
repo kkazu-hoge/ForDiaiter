9cd695ff9ec5b943f81f97108f6eb6dfe1075aa2
kkazu-hoge <928kuni@gmail.com>
2022-05-11 07:30:15 +0000

    [update]model bag fix

--- app/models/pj_event.rb
+++ app/models/pj_event.rb
@@ -1,6 +1,6 @@
 class PjEvent < ApplicationRecord
 
-  belongs_to :project
+  belongs_to :project, foreign_key => "project_id"
   has_many :pj_event_details, dependent: :destroy
   has_many :trainings, through: :pj_event_details
 
--- app/models/plan_pj_event.rb
+++ app/models/plan_pj_event.rb
@@ -1,5 +1,6 @@
 class PlanPjEvent < ApplicationRecord
 
+  belongs_to :project, foreign_key => "project_id"
   has_many :plan_pj_event_details, dependent: :destroy
   has_many :trainings,through: :plan_pj_event_details
 
