01202e680606ad9c5c3dd57f88ef976c82e8f4a6
kkazu-hoge <928kuni@gmail.com>
2022-05-16 12:26:17 +0000

    [update]project new_wizard3 making

--- app/controllers/public/projects_controller.rb
+++ app/controllers/public/projects_controller.rb
@@ -16,17 +16,17 @@ class Public::ProjectsController < Public::ApplicationController
     @project = Project.new
     #sessionに情報保持していなければ会員情報の値をセット
     if session[:project].blank?
-      @project[:sex] =           current_customer.sex
-      @project[:age] =           Customer.birthday_transfer_age(current_customer.birthday)
-      @project[:height] =        current_customer.height
-      @project[:weight] =        current_customer.weight
-      @project[:target_weight] = ""
+      @project["sex"] =           current_customer.sex
+      @project["age"] =           Customer.birthday_transfer_age(current_customer.birthday)
+      @project["height"] =        current_customer.height
+      @project["weight"] =        current_customer.weight
+      @project["target_weight"] = ""
     else
-      @project[:sex] =           session[:project]["sex"]
-      @project[:age] =           session[:project]["age"]
-      @project[:height] =        session[:project]["height"]
-      @project[:weight] =        session[:project]["weight"]
-      @project[:target_weight] = session[:project]["target_weight"]
+      @project["sex"] =           session[:project]["sex"]
+      @project["age"] =           session[:project]["age"]
+      @project["height"] =        session[:project]["height"]
+      @project["weight"] =        session[:project]["weight"]
+      @project["target_weight"] = session[:project]["target_weight"]
     end
   end
 
@@ -41,30 +41,30 @@ class Public::ProjectsController < Public::ApplicationController
 
     #基礎情報入力画面でセットしたリクエストパラメータをsession情報に保存
     unless params[:project].blank?
-      session[:project][:sex] =           params[:project][:sex]
-      session[:project][:age] =           params[:project][:age]
-      session[:project][:height] =        params[:project][:height]
-      session[:project][:weight] =        params[:project][:weight]
-      session[:project][:target_weight] = params[:project][:target_weight]
+      session[:project]["sex"] =           params[:project][:sex]
+      session[:project]["age"] =           params[:project][:age]
+      session[:project]["height"] =        params[:project][:height]
+      session[:project]["weight"] =        params[:project][:weight]
+      session[:project]["target_weight"] = params[:project][:target_weight]
     end
 
     #プロジェクト設定の値を設定
     @project = Project.new
     if session[:project]["name"].blank?
-      @project[:name] =                  ""
-      @project[:pj_start_day] =          Date.current
-      @project[:pj_finish_day] =         Date.current
-      @project[:life_stress_factor_id] = 1
-      @project[:intake_calorie_perday] = ""
-      @project[:interval] =              ""
+      @project["name"] =                  ""
+      @project["pj_start_day"] =          Date.current
+      @project["pj_finish_day"] =         Date.current
+      @project["life_stress_factor_id"] = 1
+      @project["intake_calorie_perday"] = ""
+      @project["interval"] =              ""
     else
       # binding/pry
-      @project[:name] =                  session[:project]["name"]
-      @project[:pj_start_day] =          session[:project]["pj_start_day"]
-      @project[:pj_finish_day] =         session[:project]["pj_finish_day"]
-      @project[:life_stress_factor_id] = session[:project]["life_stress_factor_id"]
-      @project[:intake_calorie_perday] = session[:project]["intake_calorie_perday"]
-      @project[:interval] =              session[:project]["interval"]
+      @project["name"] =                  session[:project]["name"]
+      @project["pj_start_day"] =          session[:project]["pj_start_day"]
+      @project["pj_finish_day"] =         session[:project]["pj_finish_day"]
+      @project["life_stress_factor_id"] = session[:project]["life_stress_factor_id"]
+      @project["intake_calorie_perday"] = session[:project]["intake_calorie_perday"]
+      @project["interval"] =              session[:project]["interval"]
     end
   end
 
@@ -77,12 +77,12 @@ class Public::ProjectsController < Public::ApplicationController
       pj_start_day = Date.new params[:project]["pj_start_day(1i)"].to_i,params[:project]["pj_start_day(2i)"].to_i,params[:project]["pj_start_day(3i)"].to_i
       pj_finish_day = Date.new params[:project]["pj_finish_day(1i)"].to_i,params[:project]["pj_finish_day(2i)"].to_i,params[:project]["pj_finish_day(3i)"].to_i
 
-      session[:project][:name] =                  params[:project][:name]
-      session[:project][:pj_start_day] =          pj_start_day
-      session[:project][:pj_finish_day] =         pj_finish_day
-      session[:project][:life_stress_factor_id] = params[:project][:life_stress_factor_id]
-      session[:project][:intake_calorie_perday] = params[:project][:intake_calorie_perday]
-      session[:project][:interval] =              params[:project][:interval]
+      session[:project]["name"] =                  params[:project][:name]
+      session[:project]["pj_start_day"] =          pj_start_day
+      session[:project]["pj_finish_day"] =         pj_finish_day
+      session[:project]["life_stress_factor_id"] = params[:project][:life_stress_factor_id]
+      session[:project]["intake_calorie_perday"] = params[:project][:intake_calorie_perday]
+      session[:project]["interval"] =              params[:project][:interval]
     end
 
     #表示用データ
