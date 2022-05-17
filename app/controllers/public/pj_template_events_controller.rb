85b27f958302dd62425d26f7a94ca48cdf779817
kkazu-hoge <928kuni@gmail.com>
2022-05-17 05:20:52 +0000

    [update]project new_wizard3 modal trainig_list add success

--- app/controllers/public/pj_template_events_controller.rb
+++ app/controllers/public/pj_template_events_controller.rb
@@ -14,21 +14,24 @@ class Public::PjTemplateEventsController < Public::ApplicationController
   end
 
   def pj_event_add_training
-    # @pj_event_details = PjEventDetail.new
-    # @trainings = Training.all
-    # hash_add_training = {"training_id"=>1, "burn_calories"=>0,"activity_minutes"=>0  }
-    # session[:pj_event_details]["#{training_id}"] = hash_add_training
 
+    #リクエストパラメータのトレーニングidからトレーニングデータを取得
     training_obj = Training.find(params[:training_id])
-    hash_add_training = {"training_id"=>training_obj.id, "activity_minutes"=>0  }
+    minutes = 10
+    calorie_per_ten_min = burn_calories_training(training_obj.mets_value, session[:project]["weight"], minutes).ceil
+    #ハッシュで１トレーニングのデータを作成する
+    hash_add_training = {"training_id"=>training_obj.id, "activity_minutes"=>0, "training_name"=>training_obj.name, "calorie_per_ten_min"=>calorie_per_ten_min }
     if session[:pj_event_details].blank?
       session[:pj_event_details] = {}
     end
+    #セッションに1トレーニング分のハッシュデータを追加する
     session[:pj_event_details]["#{training_obj.id}"] = hash_add_training
 
-    @training_name = training_obj.name
-    minutes = 10
-    @calorie_per_ten_min = burn_calories_training(training_obj.mets_value, session[:project]["weight"], minutes).ceil
+    # @training_name = training_obj.name
+
+    @pj_event_details = session[:pj_event_details]
+
+
     respond_to do |format|
       if session[:pj_event_details]["#{training_obj.id}"].blank?
         format.js { @status = "fail" }
