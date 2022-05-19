2794574c3ab2599653fc19edd473c371d3685978
kkazu-hoge <928kuni@gmail.com>
2022-05-19 04:08:10 +0000

    [update]training show view making

--- app/controllers/public/callenders_controller.rb
+++ app/controllers/public/callenders_controller.rb
@@ -2,6 +2,7 @@ class Public::CallendersController < Public::ApplicationController
   def edit
     pj_event = PjEvent.find(params[:pj_event_id])
     @pj_event_details = pj_event.pj_event_details
+    #プロジェクト新規作成時の計画情報取得
     @plan_pj_event_details = PlanPjEvent.find_by(project_id: pj_event.project_id).plan_pj_event_details
     @plan_sum_burn_cals = 0
     @plan_pj_event_details.each do |pped|
