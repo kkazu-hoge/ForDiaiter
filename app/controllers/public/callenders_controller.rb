ac6ac28005bd3d38205a2fc4c7f06343f304fc43
kkazu-hoge <928kuni@gmail.com>
2022-05-19 08:27:07 +0000

    [update]training result input function available

--- app/controllers/public/callenders_controller.rb
+++ app/controllers/public/callenders_controller.rb
@@ -1,10 +1,13 @@
 class Public::CallendersController < Public::ApplicationController
+
+   include CalorieCalcuration
+
   def edit
-    pj_event = PjEvent.find(params[:pj_event_id])
-    @pj_event_details = pj_event.pj_event_details
+    @pj_event =         PjEvent.find(params[:pj_event_id])
+    @pj_event_details = @pj_event.pj_event_details
     #プロジェクト新規作成時の計画情報取得
-    @plan_pj_event_details = PlanPjEvent.find_by(project_id: pj_event.project_id).plan_pj_event_details
-    @plan_sum_burn_cals = 0
+    @plan_pj_event_details =  PlanPjEvent.find_by(project_id: @pj_event.project_id).plan_pj_event_details
+    @plan_sum_burn_cals =     0
     @plan_pj_event_details.each do |pped|
       @plan_sum_burn_cals += pped.burn_calories
     end
@@ -26,7 +29,41 @@ class Public::CallendersController < Public::ApplicationController
   end
 
   def update
+    #プロジェクトの体重情報が必要なためproject取得
+    pj_event =  PjEvent.find(params[:pj_event_id])
+    project =   Project.find(pj_event.project_id)
+    #配列要素指定のため変数として定義
+    pj_event_details_id = 0
+    activity_minutes =    1
+    #詳細の数だけトレーニング時間、消費カロリーを保存する
+    params[:pj_event_detail].each do |ped|
+      # binding.pry
+      pj_event_detail =                   PjEventDetail.find(ped[pj_event_details_id])
+      pj_event_detail.activity_minutes =  ped[activity_minutes]
+      mets_value =                        Training.find(pj_event_detail.training_id).mets_value
+      pj_event_detail.burn_calories =     burn_calories_training(mets_value.to_f, project.weight.to_i, ped[activity_minutes].to_i)
+      pj_event_detail.save
+    end
 
+    # jsに渡す処理結果を定義
+    respond_to do |format|
+      if
+        format.js { @status = "success" }
+      else
+        format.js { @status = "fail" }
+      end
+    end
   end
+
+  private
+  # def new_params
+  #   params.require(:pj_event_detail).permit(
+  #     :sex,
+  #     :age,
+  #     :height,
+  #     :weight,
+  #     :target_weight
+  #     )
+  # end
 
 end
