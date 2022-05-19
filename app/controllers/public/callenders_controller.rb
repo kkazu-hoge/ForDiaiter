0dbe58018cfdc0a53d2a7b7f23015636b6cb808c
kkazu-hoge <928kuni@gmail.com>
2022-05-19 01:49:26 +0000

    [update]callender event edit making

--- app/controllers/public/callenders_controller.rb
+++ app/controllers/public/callenders_controller.rb
@@ -1,9 +1,12 @@
 class Public::CallendersController < Public::ApplicationController
   def edit
-    @trainings = Training.all
-    @pj_event = PjEvent.find(params[:pj_event_id])
-    @pj_event_details = 
-    
+    pj_event = PjEvent.find(params[:pj_event_id])
+    @pj_event_details = pj_event.pj_event_details
+    @plan_pj_event_details = PlanPjEvent.find_by(project_id: pj_event.project_id).plan_pj_event_details
+    @plan_sum_burn_cals = 0
+    @plan_pj_event_details.each do |pped|
+      @plan_sum_burn_cals += pped.burn_calories
+    end
   end
 
   def new
