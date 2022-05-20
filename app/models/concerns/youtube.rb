4347c8782a64698378124edc2ebe6e8b1219b2b2
kkazu-hoge <928kuni@gmail.com>
2022-05-20 01:25:52 +0000

    [update]training movie youtube api get success

--- app/models/concerns/youtube.rb
+++ app/models/concerns/youtube.rb
@@ -3,22 +3,24 @@ module Youtube
 
   require 'google/apis/youtube_v3'
 
-  GOOGLE_API_KEY = Rails.application.credentials.youtube[:api_key]
+  YOUTUBE_API_KEY = Rails.application.credentials.youtube[:api_key]
 
 
-  def find_videos(keyword, after: 1.months.ago, before: Time.now) #検索キーワードと検索範囲を変えれるように引数に値を取っています
+  def find_videos(keyword)
+  # def find_videos(keyword, after: 1.months.ago, before: Time.now)#検索キーワードと検索範囲を変えれるように引数に値を取得
     service = Google::Apis::YoutubeV3::YouTubeService.new
-    service.key = GOOGLE_API_KEY
+    service.key = YOUTUBE_API_KEY
 
     next_page_token = nil
     opt = {
       q: keyword,
       type: 'video',
-      max_results: 2,
-      order: :date,
+      max_results: 3,
+      order: :viewCount,
+      # order: :date,
       page_token: next_page_token,
-      published_after: after.iso8601,
-      published_before: before.iso8601
+      # published_after: after.iso8601,
+      # published_before: before.iso8601
     }
     results = service.list_searches(:snippet, opt)
     return results
