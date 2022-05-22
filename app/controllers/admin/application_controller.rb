8e73f85d1449944b30acf354abfd55858a5d6b1a
kkazu-hoge <928kuni@gmail.com>
2022-05-22 10:20:54 +0000

    [update]error handring exception

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -3,4 +3,6 @@ class Admin::ApplicationController < ActionController::Base
   layout 'admin/application'
   # before_action :authenticate_admin!, except: [:top, :about]
 
+  include ErrorHandlers
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -31,8 +31,6 @@ class Customer < ApplicationRecord
       customer.birthday = "2000-07-07"
       customer.height = 170
       customer.weight = 70
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
   end
 
