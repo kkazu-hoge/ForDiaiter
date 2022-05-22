ec37c6b4c5c895070843dccabc26b6da3ec45121
kkazu-hoge <928kuni@gmail.com>
2022-05-22 17:07:01 +0000

    [update]project making process refactoring

--- app/controllers/public/projects_controller.rb
+++ app/controllers/public/projects_controller.rb
@@ -1,7 +1,8 @@
 class Public::ProjectsController < Public::ApplicationController
 
-  include CalorieCalcuration
   include Common
+  include CalorieCalcuration
+  include ServiceProject
 
   before_action :initial_value_set, only: [:new]
 
@@ -13,95 +14,71 @@ class Public::ProjectsController < Public::ApplicationController
   end
 
   def new
-    @project = Project.new
-    #sessionに情報保持していなければ会員情報の値をセット
-    if session[:project].blank?
-      @project["sex"] =           current_customer.sex
-      @project["age"] =           Customer.birthday_transfer_age(current_customer.birthday)
-      @project["height"] =        current_customer.height
-      @project["weight"] =        current_customer.weight
-      @project["target_weight"] = ""
-    else
-      @project["sex"] =           session[:project]["sex"]
-      @project["age"] =           session[:project]["age"]
-      @project["height"] =        session[:project]["height"]
-      @project["weight"] =        session[:project]["weight"]
-      @project["target_weight"] = session[:project]["target_weight"]
-    end
+    project = Project.new
+    #sessionに情報保持していなければ会員情報の値をprojectにセット(ServiceProject module)
+    @project = basic_info_set(project, session[:project], current_customer)
   end
 
 
   def new_wizard2
-    #セッションにprojectモデルが紐づいていない場合はオブジェクトを生成する
-    if session[:project].blank?
-      session[:project] = Project.new
-    end
-
-    #リクエスト内容の入力チェック・バリデーションの実装が必要
-
-    #基礎情報入力画面でセットしたリクエストパラメータをsession情報に保存
+    #処理ステータスコードの定義
+    success = 0
+    #リクエスト内容の入力チェック(基礎情報入力画面からのリクエスト時のみチェック)
     unless params[:project].blank?
-      session[:project]["sex"] =           params[:project][:sex]
-      session[:project]["age"] =           params[:project][:age]
-      session[:project]["height"] =        params[:project][:height]
-      session[:project]["weight"] =        params[:project][:weight]
-      session[:project]["target_weight"] = params[:project][:target_weight]
+      if basic_info_validation(params[:project]) == success
+        #セッションにprojectが紐づいていない場合はオブジェクトを生成する
+        session[:project].blank? ? session[:project] = Project.new :
+        #基礎情報入力画面でセットしたリクエストパラメータをsession情報に保存
+        session[:project]["sex"] =           params[:project][:sex]
+        session[:project]["age"] =           params[:project][:age]
+        session[:project]["height"] =        params[:project][:height]
+        session[:project]["weight"] =        params[:project][:weight]
+        session[:project]["target_weight"] = params[:project][:target_weight]
+      else
+        redirect_to request.referer, notice: "入力されていない項目、または入力値が異常な項目があります"
+        # @project = basic_info_set(Project.new, session[:project], current_customer)
+        # render "/projects/new"
+      end
     end
 
-    #プロジェクト設定の値を設定
-    @project = Project.new
-    if session[:project]["name"].blank?
-      @project["name"] =                  ""
-      @project["pj_start_day"] =          Date.current
-      @project["pj_finish_day"] =         Date.current
-      @project["life_stress_factor_id"] = 1
-      @project["intake_calorie_perday"] = ""
-      @project["interval"] =              ""
-    else
-      @project["name"] =                  session[:project]["name"]
-      @project["pj_start_day"] =          session[:project]["pj_start_day"]
-      @project["pj_finish_day"] =         session[:project]["pj_finish_day"]
-      @project["life_stress_factor_id"] = session[:project]["life_stress_factor_id"]
-      @project["intake_calorie_perday"] = session[:project]["intake_calorie_perday"]
-      @project["interval"] =              session[:project]["interval"]
-    end
+    #プロジェクト設定画面の表示値をセット
+    project = Project.new
+    @project = project_info_set(project, session[:project])
   end
 
 
   def new_wizard3
-    #リクエスト内容の入力チェック・バリデーションの実装が必要
-
-    #プロジェクト設定画面でセットしたリクエストパラメータをsession情報に保存
+    #処理ステータスコードの定義
+    success = 0
+    #リクエスト内容の入力チェック(プロジェクト設定画面からのリクエスト時のみチェック)
     unless params[:project].blank?
-      pj_start_day = Date.new params[:project]["pj_start_day(1i)"].to_i,params[:project]["pj_start_day(2i)"].to_i,params[:project]["pj_start_day(3i)"].to_i
-      pj_finish_day = Date.new params[:project]["pj_finish_day(1i)"].to_i,params[:project]["pj_finish_day(2i)"].to_i,params[:project]["pj_finish_day(3i)"].to_i
+      if project_info_validation(params[:project], session[:project]) == success
 
-      session[:project]["name"] =                  params[:project][:name]
-      session[:project]["pj_start_day"] =          pj_start_day
-      session[:project]["pj_finish_day"] =         pj_finish_day
-      session[:project]["life_stress_factor_id"] = params[:project][:life_stress_factor_id]
-      session[:project]["intake_calorie_perday"] = params[:project][:intake_calorie_perday]
-      session[:project]["interval"] =              params[:project][:interval]
+        pj_start_day = Date.new params[:project]["pj_start_day(1i)"].to_i,params[:project]["pj_start_day(2i)"].to_i,params[:project]["pj_start_day(3i)"].to_i
+        pj_finish_day = Date.new params[:project]["pj_finish_day(1i)"].to_i,params[:project]["pj_finish_day(2i)"].to_i,params[:project]["pj_finish_day(3i)"].to_i
+        #プロジェクト設定画面でセットしたリクエストパラメータをsession情報に保存
+        session[:project]["name"] =                  params[:project][:name]
+        session[:project]["pj_start_day"] =          pj_start_day
+        session[:project]["pj_finish_day"] =         pj_finish_day
+        session[:project]["life_stress_factor_id"] = params[:project][:life_stress_factor_id]
+        session[:project]["intake_calorie_perday"] = params[:project][:intake_calorie_perday]
+        session[:project]["interval"] =              params[:project][:interval]
+      else
+        redirect_to request.referer, notice: "入力されていない項目、または入力値が異常な項目があります"
+      end
     end
 
-    #表示用データ
-    tmp_project = session[:project]
+    #トレーニング設定画面の表示値をセット
+    @weight = session[:project]["weight"]
+    @target_weight = session[:project]["target_weight"]
 
-    @weight = tmp_project["weight"]
-    @target_weight = tmp_project["target_weight"]
-
-    bmr =                           basal_metabolic_rate(tmp_project)
-    naturally_burn_calorie_perday = bmr.to_f * LifeStressFactor.find(tmp_project["life_stress_factor_id"].to_i).coefficient
-    intake_calorie_perday =         tmp_project["intake_calorie_perday"].to_f
-    diff_calorie_perday =           diff_calorie_perday(intake_calorie_perday,naturally_burn_calorie_perday)
-    pj_scope_day_counts =           day_counts(tmp_project["pj_start_day"].to_date, tmp_project["pj_finish_day"].to_date)
-    sum_diff_calorie =              diff_calorie_perday.to_i * pj_scope_day_counts.to_i
-    @target_burn_kcal =             target_burn_kcal(tmp_project, sum_diff_calorie)
-    @event_counts =                 event_counts_calc(tmp_project["pj_finish_day"].to_date, tmp_project["pj_start_day"].to_date,tmp_project["interval"].to_i)
-    @target_burn_kcal_per_event =  (@target_burn_kcal.to_f/@event_counts).ceil
-
+    tmp_project = training_info_set(session[:project])
+    @target_burn_kcal =            tmp_project["target_burn_kcal"]
+    @event_counts =                tmp_project["event_counts"]
+    @target_burn_kcal_per_event =  tmp_project["target_burn_kcal_per_event"]
 
   end
+
 
   def edit
   end
@@ -182,15 +159,13 @@ class Public::ProjectsController < Public::ApplicationController
 
 
 
-  private
-
-  def initial_value_set
+  private def initial_value_set
     @height_pulldown = height_pulldown_get  #Commonメソッド
     @weight_pulldown = weight_pulldown_get  #Commonメソッド
     @age_pulldown    = age_pulldown_get  #Commonメソッド
   end
 
-  def new_params
+  private def new_params
     params.require(:project).permit(
       :sex,
       :age,
@@ -199,5 +174,6 @@ class Public::ProjectsController < Public::ApplicationController
       :target_weight
       )
   end
+
 
 end
---
+++ app/models/concerns/service_project.rb
@@ -0,0 +1,108 @@
+module ServiceProject
+  extend ActiveSupport::Concern
+
+  #プロジェクト新規作成時の基礎情報入力の表示内容に関する処理
+  def basic_info_set(new_pj_obj, session_obj, customer)
+    if session_obj.blank?
+      new_pj_obj["sex"] =           customer["sex"]
+      new_pj_obj["age"] =           Customer.birthday_transfer_age(customer["birthday"].to_date)
+      new_pj_obj["height"] =        customer["height"]
+      new_pj_obj["weight"] =        customer["weight"]
+      new_pj_obj["target_weight"] = ""
+    else
+      new_pj_obj["sex"] =           session_obj["sex"]
+      new_pj_obj["age"] =           session_obj["age"]
+      new_pj_obj["height"] =        session_obj["height"]
+      new_pj_obj["weight"] =        session_obj["weight"]
+      new_pj_obj["target_weight"] = session_obj["target_weight"]
+    end
+    return new_pj_obj
+  end
+
+
+  #プロジェクト作成の基礎情報の入力内容チェック
+  def basic_info_validation(pj_obj)
+    status = 9 # 0：success 9: error
+    check_project = Project.new(
+      sex:                    pj_obj["sex"],
+      age:                    pj_obj["age"],
+      height:                 pj_obj["height"],
+      weight:                 pj_obj["weight"],
+      target_weight:          pj_obj["target_weight"],
+      customer_id:            current_customer.id,      #バリデーションチェックのため仮の値を設定
+      life_stress_factor_id:  1,                        #バリデーションチェックのため仮の値を設定
+      pj_start_day:           Date.current,             #バリデーションチェックのため仮の値を設定
+      pj_finish_day:          Date.current+1,           #バリデーションチェックのため仮の値を設定
+      interval:               -999,                     #バリデーションチェックのため仮の値を設定
+      name:                   "チェック",               #バリデーションチェックのため仮の値を設定
+      intake_calorie_perday:  -999                      #バリデーションチェックのため仮の値を設定
+      )
+
+    status = 0 if check_project.valid?
+    return status
+  end
+
+
+  #プロジェクト新規作成時のプロジェクト設定入力の表示内容に関する処理
+  def project_info_set(new_pj_obj, session_obj)
+    if session_obj["name"].blank?
+      new_pj_obj["name"] =                  ""
+      new_pj_obj["pj_start_day"] =          Date.current
+      new_pj_obj["pj_finish_day"] =         Date.current
+      new_pj_obj["life_stress_factor_id"] = 1
+      new_pj_obj["intake_calorie_perday"] = ""
+      new_pj_obj["interval"] =              ""
+    else
+      new_pj_obj["name"] =                  session_obj["name"]
+      new_pj_obj["pj_start_day"] =          session_obj["pj_start_day"]
+      new_pj_obj["pj_finish_day"] =         session_obj["pj_finish_day"]
+      new_pj_obj["life_stress_factor_id"] = session_obj["life_stress_factor_id"]
+      new_pj_obj["intake_calorie_perday"] = session_obj["intake_calorie_perday"]
+      new_pj_obj["interval"] =              session_obj["interval"]
+    end
+    return new_pj_obj
+  end
+
+
+  #プロジェクト作成のプロジェクト設定の入力内容チェック
+  def project_info_validation(pj_obj, session_obj)
+    status = 9 # 0：success 9: error
+    pj_start_day = Date.new pj_obj["pj_start_day(1i)"].to_i, pj_obj["pj_start_day(2i)"].to_i, pj_obj["pj_start_day(3i)"].to_i
+    pj_finish_day = Date.new pj_obj["pj_finish_day(1i)"].to_i, pj_obj["pj_finish_day(2i)"].to_i, pj_obj["pj_finish_day(3i)"].to_i
+    check_project = Project.new(
+      sex:                    session_obj["sex"],
+      age:                    session_obj["age"],
+      height:                 session_obj["height"],
+      weight:                 session_obj["weight"],
+      target_weight:          session_obj["target_weight"],
+      customer_id:            current_customer.id,
+      life_stress_factor_id:  pj_obj["life_stress_factor_id"],
+      pj_start_day:           pj_start_day,
+      pj_finish_day:          pj_finish_day,
+      interval:               pj_obj["interval"],
+      name:                   pj_obj["name"],
+      intake_calorie_perday:  pj_obj["intake_calorie_perday"]
+      )
+
+    status = 0 if check_project.valid?
+    return status
+  end
+
+
+  def training_info_set(session_obj)
+    result = {}
+    bmr =                           basal_metabolic_rate(session_obj)
+    naturally_burn_calorie_perday = bmr.to_f * LifeStressFactor.find(session_obj["life_stress_factor_id"].to_i).coefficient
+    intake_calorie_perday =         session_obj["intake_calorie_perday"].to_f
+    diff_calorie_perday =           diff_calorie_perday(intake_calorie_perday,naturally_burn_calorie_perday)
+    pj_scope_day_counts =           day_counts(session_obj["pj_start_day"].to_date, session_obj["pj_finish_day"].to_date)
+    sum_diff_calorie =              diff_calorie_perday.to_i * pj_scope_day_counts.to_i
+    result["target_burn_kcal"] =             target_burn_kcal(session_obj, sum_diff_calorie)
+    result["event_counts"] =                 event_counts_calc(session_obj["pj_finish_day"].to_date, session_obj["pj_start_day"].to_date, session_obj["interval"].to_i)
+    result["target_burn_kcal_per_event"] =  (result["target_burn_kcal"].to_f/result["event_counts"]).ceil
+    binding.pry
+    return result
+  end
+
+
+end
