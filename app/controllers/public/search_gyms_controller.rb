5089dcabd571acdcd938f9a32cd144a2a8052d21
kkazu-hoge <928kuni@gmail.com>
2022-05-25 11:57:55 +0000

    [update]map search place success

--- app/controllers/public/search_gyms_controller.rb
+++ app/controllers/public/search_gyms_controller.rb
@@ -1,9 +1,9 @@
 class Public::SearchGymsController < Public::ApplicationController
   def index
-    results = Geocoder.search("東京")
-    @lat = results.first.coordinates[0]
-    @lng = results.first.coordinates[1]
-    # binding.pry
+    # results = Geocoder.search("東京")
+    # @lat = results.first.coordinates[0]
+    # @lng = results.first.coordinates[1]
+
   end
 
   def search
