df5663993d6e0bc1deb8e18f332c8e0170db7cbc
kkazu-hoge <928kuni@gmail.com>
2022-05-26 14:59:23 +0000

    [update]search gym marker disp success

--- app/controllers/public/search_gyms_controller.rb
+++ app/controllers/public/search_gyms_controller.rb
@@ -12,8 +12,8 @@ class Public::SearchGymsController < Public::ApplicationController
       @map_center_lat = results.first.coordinates[0]
       @map_center_lng = results.first.coordinates[1]
       client = GooglePlaces::Client.new(Rails.application.credentials.googlemap[:api_key])
-      @places = client.spots(@map_center_lat, @map_center_lng, :language => 'ja', :types => 'gym', :radius => 1000)
-      if @places.blank?
+      places = client.spots(@map_center_lat, @map_center_lng, :language => 'ja', :types => 'gym', :radius => 1000)
+      if places.blank?
         redirect_to request.referer, notice: "半径1km以内にジムが見つかりませんでした。条件を変更して再度検索ください。"
       end
     end
@@ -21,13 +21,12 @@ class Public::SearchGymsController < Public::ApplicationController
     # 3次元hashの初期化
     @marker_places = Hash.new { |h,k| h[k] = Hash.new(&h.default_proc) }
     i = 0
-    @places.each do |place|
+    places.each do |place|
       @marker_places[i]["name"] = place["name"]
       @marker_places[i]["lat"] = place["lat"]
       @marker_places[i]["lng"] = place["lng"]
       @marker_places[i]["rating"] = place["rating"]
       @marker_places[i]["vicinity"] = place["json_result_object"]["vicinity"]
-      # binding.pry
       unless place.photos[0].blank?
         @marker_places[i]["photo_image"] = place.photos[0].fetch_url(200)
       else
@@ -38,7 +37,6 @@ class Public::SearchGymsController < Public::ApplicationController
 
     @array_marker_places = @marker_places.to_a
     gon.array_marker_places = @array_marker_places
-    # binding.pry
   end
 
 end
