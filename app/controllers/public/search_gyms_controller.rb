472bc4e9b327a688c489c630bd5d47b1026d0309
kkazu-hoge <928kuni@gmail.com>
2022-05-26 13:50:32 +0000

    [update]search gym marker disp success

--- app/controllers/public/search_gyms_controller.rb
+++ app/controllers/public/search_gyms_controller.rb
@@ -4,7 +4,6 @@ class Public::SearchGymsController < Public::ApplicationController
 
   def search
     @center_name = params[:place_name]
-    # binding.pry
     results = Geocoder.search(@center_name) #本番ではエラーが発生する(原因は本番だとhttp通信でセキュアでないためAPI側で許可されない)
     if results.blank?
       redirect_to request.referer, notice: "検索対象の場所情報を取得できませんでした。条件を変更して再度検索ください。"
@@ -17,21 +16,31 @@ class Public::SearchGymsController < Public::ApplicationController
       if @places.blank?
         redirect_to request.referer, notice: "半径1km以内にジムが見つかりませんでした。条件を変更して再度検索ください。"
       end
-
     end
-    # places = {}
-    # i = 0
-    # @places.each do |place|
-    #   places[i]["name"] = place["name"]
-    #   places[i]["lat"] = place["lat"]
-    #   places[i]["lng"] = place["lng"]
-    #   places[i]["rating"] = place["rating"]
-    #   places[i]["photo"] = place.photos[0].fetch_url(400)
-    #   places[i]["vicinity"] = place["json_result_object"]["vicinity"]
-    #   places[i]["compound_code"] = place["json_result_object"]["plus_code"]["compound_code"]
-    #   i++
-    # end
 
+    # 3次元hashの初期化
+    @marker_places = Hash.new { |h,k| h[k] = Hash.new(&h.default_proc) }
+    i = 0
+    @places.each do |place|
+      @marker_places[i]["name"] = place["name"]
+      @marker_places[i]["lat"] = place["lat"]
+      @marker_places[i]["lng"] = place["lng"]
+      @marker_places[i]["rating"] = place["rating"]
+      @marker_places[i]["vicinity"] = place["json_result_object"]["vicinity"]
+
+      # @marker_places[i]["name"].push(place["name"])
+      # @marker_places[i]["lat"].push(place["lat"])
+      # @marker_places[i]["lng"].push(place["lng"])
+      # @marker_places[i]["rating"].push(place["rating"])
+      # # @marker_places[i]["photo"].push(place.photos[0].fetch_url(200)
+      # @marker_places[i]["vicinity"].push(place["json_result_object"]["vicinity"])
+      # # @marker_places[i]["compound_code"].push( place["json_result_object"]["plus_code"]["compound_code"]
+      i = i+1
+    end
+
+    @array_marker_places = @marker_places.to_a
+    gon.array_marker_places = @array_marker_places
+    # binding.pry
   end
 
 end
