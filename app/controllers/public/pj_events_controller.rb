2ca93b0c696a4ff4a54b942e498945b718bc02dc
kkazu-hoge <928kuni@gmail.com>
2022-05-29 08:51:15 +0000

    [update]callender error handling alter

--- app/controllers/public/pj_events_controller.rb
+++ app/controllers/public/pj_events_controller.rb
@@ -5,6 +5,8 @@ class Public::PjEventsController < Public::ApplicationController
   def show
   end
 
+
+
   def new
     @project_id = params[:project_id]
     # if session[:pj_event_details_new]["action_day"].blank?
@@ -15,18 +17,37 @@ class Public::PjEventsController < Public::ApplicationController
     # end
   end
 
+
+
   def edit
   end
+
+
 
   def update
   end
 
-  def create
 
+
+  def create
     project = Project.find(params[:project_id])
     action_day = params[:action_day].to_date
+    #処理コード定義
+    success = 0
+    action_day_pj_scope_error = 9
+    action_day_event_duplicate_error = 8
+    #リクエストパラメータチェック
+    status = success
+    status = action_day_pj_scope_error if action_day < project.pj_start_day.to_date || action_day > project.pj_finish_day.to_date
+    if status == success
+      check_pj_events = PjEvent.where(project_id: project.id)
+      check_pj_events.each do |cpe|
+        status = action_day_event_duplicate_error if action_day == cpe.action_day.to_date
+      end
+    end
 
-    # ActiveRecord::Base.transaction do
+    if status == success
+     #ActiveRecord::Base.transaction do
       # pj_eventを保存する
       pj_event = PjEvent.new(project_id: project.id)
       pj_event[:action_day] = action_day
@@ -35,23 +56,34 @@ class Public::PjEventsController < Public::ApplicationController
       # pj_event_detailsを保存する
       session[:pj_event_details_new]
       session[:pj_event_details_new].each do |ped|
-
     	  pj_event_details = PjEventDetail.new
         pj_event_details[:pj_event_id] = pj_event.id
       	pj_event_details[:training_id] = ped[1]["training_id"]
       	training = Training.find(pj_event_details[:training_id].to_i)
-    	  pj_event_details[:activity_minutes] = params[training.id.to_s]
+      	#送信されてきた運動時間が数字以外もしくは入力がない場合は0でイベント詳細を保存する
+      	if params[training.id.to_s].blank?
+          pj_event_details[:activity_minutes] = 0
+        else
+          pj_event_details[:activity_minutes] = params[training.id.to_s]
+        end
     	  pj_event_details[:burn_calories] = burn_calories_training(training.mets_value, project["weight"], pj_event_details[:activity_minutes].to_i )
     	  pj_event_details.save!
       end
-    # end
+    #end
+      #session情報をクリアする
+      session[:pj_event_details_new].clear
+      redirect_to callender_path(id: current_customer.id)
 
-    # session情報をクリアする
-    session[:pj_event_details_new].clear
-
-    redirect_to callender_path(id: current_customer.id)
-
+    elsif status == action_day_pj_scope_error
+      redirect_to request.referer, notice: "イベント実施日はプロジェクト期間内で設定ください"
+    elsif status == action_day_event_duplicate_error
+      redirect_to request.referer, notice: "イベント実施日が重複しています。1日1イベント単位で設定ください。"
+    else
+      redirect_to request.referer, notice: "システムエラーです。管理者に問い合わせてください。"
+    end
   end
+
+
 
   def destroy
     # binding.pry
@@ -61,14 +93,17 @@ class Public::PjEventsController < Public::ApplicationController
     else
       redirect_to callender_path(id: current_customer.id), notice: "イベントを削除できませんでした(#{pj_event.action_day.strftime("%m/%d")})"
     end
-
   end
+
+
 
   def new_training
     @pj_event_details = {}
     @trainings = Training.all
     @project_id = params["project_id"]
   end
+
+
 
   def pj_event_add_training_new
     #リクエストパラメータのトレーニングidからトレーニングデータを取得
@@ -95,6 +130,8 @@ class Public::PjEventsController < Public::ApplicationController
       end
     end
   end
+
+
 
   def pj_event_delete_training_new
     #セッションから該当の情報を削除する
