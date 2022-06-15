b1f3e2199f31b9b199dffd50d063bd5fc6131a96
kkazu-hoge <928kuni@gmail.com>
2022-06-15 10:49:40 +0000

    [add]bag fix

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,10 +1,5 @@
 class Column < ApplicationRecord
 
-  # 一旦保留
-  # belongs_to:admin
-
-  has_one_attached:image
-
   validates :title, :summary, presence: true
 
 end
--- app/models/concerns/newsapi.rb
+++ app/models/concerns/newsapi.rb
@@ -5,7 +5,7 @@ module Newsapi
 
   def find_news
     newsapi = News.new(Rails.application.credentials.newsapi[:api_key])
-    #URLエンコードの値は"ダイエット"
+    #URIエンコードの値は"ダイエット"
     news_headlines = newsapi.get_everything(q: '%E3%83%80%E3%82%A4%E3%82%A8%E3%83%83%E3%83%88',
                                           searchIn: 'description',
                                           from: (Date.current << 1).strftime("%Y-%m-%d"),
