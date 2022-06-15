b544d3620766a53e48208c58ff19e44c470eafc3
kkazu-hoge <928kuni@gmail.com>
2022-06-15 16:43:49 +0000

    [update]delete graph

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -4,19 +4,19 @@ class Public::ContactsController < Public::ApplicationController
 
   def new
     @contact = Contact.new
+    @customer_id = current_customer.id
   end
 
   def create
+    @customer_id = params[:contact][:customer_id]
     @contact = Contact.new(contact_params)
     if @contact.save
       ContactMailer.contact_mail(@contact, Customer.find(@contact.customer_id)).deliver
-      redirect_to root_path, notice: "お問い合わせ内容を送信しました"
+      redirect_to home_path, notice: "お問い合わせ内容を送信しました"
     else
       render :new
     end
   end
-
-
 
 
 
