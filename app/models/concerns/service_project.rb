7a4f407909167a54013aefa52f9535bed05e74fd
kkazu-hoge <928kuni@gmail.com>
2022-06-19 16:49:27 +0000

    [update]project save logic alter

--- app/models/concerns/service_project.rb
+++ app/models/concerns/service_project.rb
@@ -90,7 +90,7 @@ module ServiceProject
       )
 
     status = 0 if check_project.valid?
-    # binding.pry
+
     if status == 0
       status = 8 if pj_obj["pj_finish_day"].to_date <= pj_obj["pj_start_day"].to_date || pj_obj["pj_start_day"].to_date < Date.current
       status = 7 if pj_obj["intake_calorie_perday"].to_s.length < 3 || pj_obj["intake_calorie_perday"].to_s.length > 4
@@ -133,6 +133,64 @@ module ServiceProject
     end
 
     return status
+  end
+
+
+  def project_save_transaction(session_project, session_pj_event_details)
+    ActiveRecord::Base.transaction do
+      #１ projectを保存する
+      project = Project.new
+      project[:customer_id] =           current_customer.id
+      project[:sex] =                   session_project["sex"]
+      project[:age]	=		                session_project["age"].to_i
+      project[:height] =	              session_project["height"].to_i
+      project[:weight] =                session_project["weight"].to_i
+      project[:target_weight] =         session_project["target_weight"].to_i
+      project[:pj_start_day] =          session_project["pj_start_day"].to_date
+      project[:name] =                  session_project["name"]
+      project[:pj_finish_day] =         session_project["pj_finish_day"].to_date
+      project[:life_stress_factor_id] = session_project["life_stress_factor_id"].to_i
+      project[:intake_calorie_perday] = session_project["intake_calorie_perday"].to_i
+      project[:interval] =              session_project["interval"].to_i
+      project.save!
+
+      #２ plan_pj_eventsを保存する
+      plan_pj_event = PlanPjEvent.new(project_id: project.id)
+      plan_pj_event.save!
+
+      #３ plan_pj_event_detailsを保存する
+      session_pj_event_details.each do |ped|
+        plan_pj_event_details =                     PlanPjEventDetail.new
+        plan_pj_event_details[:plan_pj_event_id] =  plan_pj_event.id
+        plan_pj_event_details[:training_id] =       ped[1]["training_id"]
+        plan_pj_event_details[:activity_minutes] =  params[ped[1]["training_id"].to_s]
+        training =                                  Training.find(plan_pj_event_details[:training_id].to_i)
+        plan_pj_event_details[:burn_calories] =     burn_calories_training(training.mets_value, project[:weight], plan_pj_event_details[:activity_minutes] )
+
+        plan_pj_event_details.save!
+      end
+
+      #４ pj_events、pj_event_detailsを保存する
+      event_counts = event_counts_calc(project[:pj_finish_day], project[:pj_start_day], project[:interval])
+      num = 1
+      action_day = project[:pj_start_day]
+      while num <= event_counts do
+      	pj_event =              PjEvent.new(project_id: project.id)
+      	pj_event[:action_day] = action_day
+      	pj_event[:start_time] = pj_event[:action_day].to_time.to_datetime
+      	pj_event.save!
+
+      	session_pj_event_details.each do |ped|
+      	  pj_event_details =               PjEventDetail.new
+          pj_event_details[:pj_event_id] = pj_event.id
+        	pj_event_details[:training_id] = ped[1]["training_id"]
+      	  pj_event_details.save!
+        end
+
+        action_day = action_day.days_since(project[:interval])
+      	num +=1
+      end
+    end
   end
 
 
