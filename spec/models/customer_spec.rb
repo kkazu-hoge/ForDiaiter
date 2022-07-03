65e2d38e5719824e24a72dcf80805286daab66d7
kkazu-hoge <928kuni@gmail.com>
2022-07-03 14:44:08 +0000

    [update]rspec test code add

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,7 +4,7 @@ describe "会員モデルに関するテスト"  do
 
   #正常ケース
   context "性別：男性の全データが正常に設定されている場合" do
-    it "データが正常に登録される" do
+    it "データが正常に登録されること" do
       customer = FactoryBot.build(:customer)
       customer[:sex] = "man"
       expect(customer).to be_valid
@@ -12,13 +12,31 @@ describe "会員モデルに関するテスト"  do
   end
 
   context "性別：女性の全データが正常に設定されている場合" do
-    it "データが正常に登録される" do
+    it "データが正常に登録されること" do
       customer = FactoryBot.build(:customer)
       customer[:sex] = "woman"
       expect(customer).to be_valid
     end
   end
 
-  #
+  #異常ケース
+  context "場合" do
+    it "データが正常に登録されること" do
+      customer = FactoryBot.build(:customer)
+      customer[:last_name] = ""
+      customer.valid?
+      expect(customer.errors.full_messages).to include("名前を入力してください")
+    end
+  end
+  
+  context "性別：男性の全データが正常に設定されている場合" do
+    it "データが正常に登録されること" do
+      customer = FactoryBot.build(:customer)
+      customer[:first_name] = ""
+      expect(customer).to be_valid
+    end
+  end
+  
+  :public_name
 
 end
