266cfea36ceb7428ebd92d28494024fad31c2e56
kkazu-hoge <928kuni@gmail.com>
2022-07-19 13:23:13 +0000

    [update]mypage routes alter

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -3,7 +3,7 @@ class Public::ApplicationController < ActionController::Base
   layout 'public/application'
   before_action :authenticate_customer!, except: [:top, :about, :term_of_service, :privacy_policie]
 
-  include ErrorHandlers
+  # include ErrorHandlers
 
 
 end
